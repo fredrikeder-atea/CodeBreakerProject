959e7c8b19f27bca5b84bc4792c363753b5d6369
Fredrik <fredrik@Fredriks-MacBook-Pro-2.local>
2018-01-03 15:15:30 +0100

    Up to 8

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,15 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer == '' || attempt || '') {
+        setHiddenFields();
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+    answer = Math.floor(Math.random() * (10000));
+    while (answer.toString().length < 4) {
+        answer = "0" + answer;
+    }
+    $("#attempt").val(0);
+}
